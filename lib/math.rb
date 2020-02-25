f46682e5217f5994da16b48b7aca427ccde06537
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-02-25 18:48:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum
 end
 
 def subtraction(num1, num2)
-
+  dif = num1 - num2
+  return dif
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
+  return remainder
 end
 
 def square_root(num)
-
+  root = num ** (0.5)
+  return root
 end
